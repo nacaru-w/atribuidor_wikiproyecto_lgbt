53afe3f892d9fab8649f2ff29a74925df6cf40d5
nacaru-w <ignaciocr92@gmail.com>
2026-08-17 19:44:21 +0200

    [NEW] Application link references setup

---
+++ src/config/links.ts
@@ -0,0 +1,11 @@
+export const links = {
+    /** Page listing the articles created by the wikiproject */
+    articleList: 'Wikiproyecto:LGBT/Artículos creados',
+    // TODO: confirm the exact title of the counter page
+    /** Page holding the created-articles counter */
+    articleCounter: 'Wikipedia:Wikiproyecto LGBT+/Contador de artículos creados',
+    /** Wikiproject logo shown on the trigger button */
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Barba-wikiproyecto-lgbt.svg',
+    /** Spinner shown while the created-articles list is being checked */
+    spinner: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Ajax_loader_metal_512.gif',
+} as const;
